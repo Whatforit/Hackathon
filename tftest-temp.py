0dbc88f6ee3adea887a864bc267d74d46c69072d
Emerson Iaquinto <emersoniaquinto@gmail.com>
2022-10-14 16:55:18 -0400

    updates

--- tftest-temp.py
+++ tftest-temp.py
@@ -11,10 +11,10 @@ np.set_printoptions(precision=3, suppress=True)
 
 
 file = 'owid-covid-data-large.csv'
-target = 'new_cases_smoothed_per_million'
-threshhold = 4
-node_scalar = 5
-epochs = 500
+target = 'total_deaths_per_million'
+threshhold = 2
+node_scalar = 4
+epochs = 5000
 
 
 def clean_data(raw_dataset, drop=True, impute=True):
@@ -24,7 +24,7 @@ def clean_data(raw_dataset, drop=True, impute=True):
     dataset.isna().sum()
     #dataset = dataset.dropna(axis=1, how='all')
     #dataset = dataset.dropna(axis=0, thresh=19)
-    to_drop_horizontally = [
+    drop_horizontally = [
         'people_vaccinated_per_hundred',
         'total_boosters_per_hundred',
         'stringency_index',
@@ -37,17 +37,9 @@ def clean_data(raw_dataset, drop=True, impute=True):
         'male_smokers', # same reasoning ^
         'hospital_beds_per_thousand',
         'life_expectancy',
-        'human_development_index',
-        '
-        
-        
-
-        
-        
-        #'total_vaccinations_per_hundred',
-        #'people_fully_vaccinated_per_hundred',
-        
-    ]
+        'human_development_index'
+        ]
+    
     
     data_to_impute_logically = [
         'total_boosters_per_hundred'
@@ -65,16 +57,16 @@ def clean_data(raw_dataset, drop=True, impute=True):
          'new_deaths_smoothed',
          'total_cases_per_million',
          'new_cases_smoothed_per_million',
-         'total_deaths_per_million',
+         #'total_deaths_per_million',
          'new_deaths_per_million',
-         #'new_deaths_smoothed_per_million',
-         #'reproduction_rate',
+         'new_deaths_smoothed_per_million',
+         'reproduction_rate',
          'icu_patients',
          'icu_patients_per_million',
          'weekly_icu_admissions',
          'weekly_icu_admissions_per_million',
          'hosp_patients',
-         'hosp_patients_per_million'
+         'hosp_patients_per_million',
          'weekly_hosp_admissions',
          'weekly_hosp_admissions_per_million',
          'total_tests',
@@ -92,7 +84,7 @@ def clean_data(raw_dataset, drop=True, impute=True):
          'people_fully_vaccinated',
          'total_boosters',
          'new_vaccinations',
-         'new_vaccinations_smoothed',
+         #'new_vaccinations_smoothed',
          'total_vaccinations_per_hundred',
          #'people_vaccinated_per_hundred',
          'people_fully_vaccinated_per_hundred',
@@ -105,18 +97,21 @@ def clean_data(raw_dataset, drop=True, impute=True):
          'excess_mortality_cumulative_absolute',
          'excess_mortality_cumulative',
          'excess_mortality',
-         'excess_mortality_cumulative_per_million'
+         'excess_mortality_cumulative_per_million',
+         'aged_70_older',
         ], axis=1)
     print(len(dataset.columns))
     dataset = pd.get_dummies(
-        dataset, columns=['location'], prefix='', prefix_sep='')
+        dataset, columns=['continent'], prefix='', prefix_sep='')
     if drop:
         dataset = dataset.dropna(subset=[target])
-        dataset = dataset.dropna(thresh=dataset.shape[1]-threshhold, axis=0)
+        dataset = dataset.dropna(subset=drop_horizontally)
+        #dataset = dataset.dropna(thresh=dataset.shape[1]-threshhold, axis=0)
+        print(dataset.loc[:, dataset.isna().any()])
     if impute:
         impute_columns = dataset.columns
         for column in impute_columns:
-            dataset[column] = dataset[column].fillna(dataset[column].median())
+            dataset[column] = dataset[column].fillna(dataset[column].mean())
     return (dataset)
 
 
@@ -158,7 +153,7 @@ def build_and_compile_model(norm):
     model = keras.Sequential([
         norm,
         layers.Dense(nodes, activation='relu',),
-        layers.Dropout(0.2),
+        layers.Dropout(0.5),
         layers.Dense(1)
     ])
 
@@ -180,7 +175,7 @@ history = dnn_model.fit(
     train_features,
     train_labels,
     validation_split=0.2,
-    verbose=1, epochs=epochs, callbacks=[earlystopping])
+    verbose=1, epochs=epochs)
 
 
 # plot_loss(history)
@@ -193,11 +188,14 @@ test_results['dnn_model'] = dnn_model.evaluate(
 # Predict
 test_predictions = dnn_model.predict(test_features).flatten()
 differences = []
-
+percent_diff = []
 
 def test_model(test_labels, test_predictions):
     for actual, predicted in zip(test_labels, test_predictions):
         diff = abs(actual - predicted)
+        if actual != 0:
+            percent_error = (diff / actual) * 100
+            percent_diff.append(percent_error)
         print(
             f"Actual: {actual} ===== Predicted: {predicted} ===== Difference: {diff}")
         differences.append(diff)
@@ -206,10 +204,9 @@ def test_model(test_labels, test_predictions):
 
 
 test_model(test_labels, test_predictions)
-average_diff = sum(differences)/len(differences)
-percent_diff = average_diff / sum(test_labels) * 100
-print(f"Percent Difference: {percent_diff}")
-print(f"Average difference: {average_diff}")
+
+percent_diff = np.array(percent_diff).mean()
+print(f"Average difference: {percent_diff}")
 print(f"Max difference: {max(differences)}")
 print(f"Min difference: {min(differences)}")
 
